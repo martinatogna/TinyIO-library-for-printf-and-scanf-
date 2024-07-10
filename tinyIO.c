35044a738a61d5135dfb33e5eeec75e02ef6b5cc
martinatogna <101289379+martinatogna@users.noreply.github.com>
2024-07-10 12:02:45 +0200

    Delete tinyIO.c

--- tinyIO.c
+++
@@ -1,9 +0,0 @@
-#include "tinyIO.h"
-
-int32_t uart_init(uint32_t baseaddr) {
-
-    global_uart = (uart_t *) (intptr_t) baseaddr + UART_RX_FIFO_REG ;
-    global_uart->ctrl_reg = UART_RX_RESET | UART_TX_RESET;	
-
-	return 0;
-}
